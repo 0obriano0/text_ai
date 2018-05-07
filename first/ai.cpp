4dc8d35c9d05ee8080520e55b2044f80ce3a0fc4
0obriano0 <37872600+0obriano0@users.noreply.github.com>
2018-05-08 00:50:39 +0800

    優化程式碼

--- first/ai.cpp
+++ first/ai.cpp
@@ -5,7 +5,7 @@
 char loading_data[] = "load.txt";
 char test_data[] = "test.txt";
 
-double th = 5;
+double th = 4.533;
 
 int main(){
 	FILE *pf;
@@ -18,7 +18,7 @@ int main(){
 			double data[2];
 			double answer;
 			fscanf(pf,"%lf%lf",&data[0],&data[1]);
-			printf("判定 %lf 有沒有倒 %lf\n",data[0],data[1]);
+			//printf("判定 %lf 有沒有倒 %lf\n",data[0],data[1]);
 			if(data[0] <= th){
 				answer = 0;
 			}else{
@@ -31,16 +31,16 @@ int main(){
 					answer = 1;
 				}
 				if(data[0] <= th){
-					th-=0.1;
+					th-=0.000001;
 				}else{
-					th+=0.1;
+					th+=0.000001;
 				}
-				printf("目前 th =  %lf\n",th);
+				//printf("目前 th =  %lf\n",th);
 			}
 		}
 		printf("------------------------------------------------\n");
 		fclose(pf);
-		//printf("th = %lf \n",th);
+		printf("th = %lf \n",th);
 	}
 	int true_num = 0;
 	int false_num = 0;
@@ -55,17 +55,23 @@ int main(){
 			double data[3];
 			double answer;
 			fscanf(pf,"%lf%lf",&data[0],&data[1]);
-			printf("判定 %lf 有沒有倒 %lf\n",data[0],data[1]);
+			//printf("判定 %lf 有沒有倒 %lf\n",data[0],data[1]);
 			if(data[0] <= th){
 				data[2] = 0;
 			}else{
 				data[2] = 1;
 			}
-			fprintf(pf2,"%.3lf %.0lf %.0lf\n",data[0],data[1],data[2]);
-			if(data[2] == data[1])
+			fprintf(pf2,"%.6lf %.0lf %.0lf\n",data[0],data[1],data[2]);
+			if(data[2] == data[1]){
 				true_num += 1;
-			else
+			}else{
 				false_num +=1;
+				printf("判定 %lf",data[0]);
+				if(data[2])
+					printf("有倒 正確的值為 沒倒\n");
+				else
+					printf("沒倒 正確的值為 有倒\n");
+			}
 		}
 		printf("------------------------------------------------\n");
 		fclose(pf);
--- first/create.cpp
+++ first/create.cpp
@@ -12,57 +12,46 @@ double th = 5;
 
 int num = 50000;
 
+void create_data(char *s);
+
 int main(){
 	printf("請輸入規則th=");
 	scanf("%lf",&th);
-	FILE *pf;
-	pf = fopen(loading_data,"w");
-	if(pf == NULL){
-			printf("無法開啟檔案");
-			return 0;
-	}else{
-		printf("\n------------------檔案設定完成------------------\n");
-		srand((unsigned)time(NULL));
-		for(int loopnum1 = 0; loopnum1 < num;loopnum1++){
-			int data1 = rand()%9+1;
-			int data2 = rand()%1000;
-			double data[2];
-			data[0] = data1 + data2*0.001;
-			if(data[0]<=th){
-				data[1] = 0;
-			}else{
-				data[1] = 1;
-			}
-			fprintf(pf,"%.3lf %.3lf\n",data[0],data[1]);
-		}
-		printf("------------------------------------------------\n");
-		fclose(pf);
-	}
-	Sleep(1000+th);
+	printf("請輸入數量:");
+	scanf("%d",&num);
 	
-	pf = fopen(test_data,"w");
-	if(pf == NULL){
-			printf("無法開啟檔案");
-			return 0;
-	}else{
-		printf("\n------------------檔案設定完成------------------\n");
-		srand((unsigned)time(NULL));
-		for(int loopnum1 = 0; loopnum1 < num;loopnum1++){
-			int data1 = rand()%9+1;
-			int data2 = rand()%1000;
-			double data[2];
-			data[0] = data1 + data2*0.001;
-			if(data[0]<=th){
-				data[1] = 0;
-			}else{
-				data[1] = 1;
-			}
-			fprintf(pf,"%.3lf %.3lf\n",data[0],data[1]);
-		}
-		printf("------------------------------------------------\n");
-		fclose(pf);
-	}
+	create_data(loading_data);
+	Sleep(1000+th);
+	create_data(test_data); 
+	
 	printf("th = %lf \n",th);
 	system("pause");
 	return 0;
+}
+
+void create_data(char *s){
+	FILE *pf;
+	pf = fopen(s,"w");
+	if(pf == NULL){
+			printf("無法開啟檔案");
+			return 0;
+	}else{
+		printf("\n------------------檔案設定完成------------------\n");
+		srand((unsigned)time(NULL));
+		for(int loopnum1 = 0; loopnum1 < num;loopnum1++){
+			int data1 = rand()%9+1;
+			int data2 = rand()%1000;
+			int data3 = rand()%1000;
+			double data[2];
+			data[0] = data1 + data2*0.001 + data3*0.000001;
+			if(data[0]<=th){
+				data[1] = 0;
+			}else{
+				data[1] = 1;
+			}
+			fprintf(pf,"%.6lf %.6lf\n",data[0],data[1]);
+		}
+		printf("------------------------------------------------\n");
+		fclose(pf);
+	}
 }
